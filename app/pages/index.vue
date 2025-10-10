897d60c8e5896976f23301dcf8dcb906c7b7a2f2
doniahusien <doniahusien@gmail.com>
2025-10-10 04:08:27 +0300

    Enhance home page layout and content

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,12 +1,11 @@
-<script setup>
-const { locales, setLocale } = useI18n()
-</script>
-
 <template>
-  <div>
-    <button v-for="locale in locales" @click="setLocale(locale.code)">
-      {{ locale.name }}
-    </button>
-    <h1>{{ $t('nav.home') }}</h1>
-  </div>
+  <section class="relative">
+    <Hero />
+    <div class="absolute bottom-[-4rem] left-1/2 -translate-x-1/2 w-[95%] z-20">
+      <HighlightsSection />
+    </div>
+  </section>
+  
+  <AboutSection />
+   
 </template>
