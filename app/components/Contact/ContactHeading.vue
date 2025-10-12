87d1a1da02814a87b712f318ade98d41f43d3007
doniahusien <doniahusien@gmail.com>
2025-10-12 16:17:45 +0300

    feat(contact): add ContactHeading component for page header section

---
+++ app/components/Contact/ContactHeading.vue
@@ -0,0 +1,10 @@
+<script setup>
+const { t } = useI18n()
+</script>
+<template>
+  <div>
+    <BaseHeading :title="$t('contact.title')" />
+    <BaseParagraph :title="$t('contact.description')" classes="text-soft-gray" />
+  </div>
+</template>
+
