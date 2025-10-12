93a52f4d4cba8c0d0f753f5ee49c2721f02dc3e1
doniahusien <doniahusien@gmail.com>
2025-10-12 15:43:46 +0300

    feat(page): redesign Services page and integrate BaseHero

--- app/pages/services.vue
+++ app/pages/services.vue
@@ -1,1 +1,13 @@
-<template></template>
+<template>
+  <BaseHero
+    :title="t('services.title')"
+    :subtitle="t('services.subtitle')"
+    bgImage= "bg-[url('/images/servicesheader.jpg')]"
+  />
+  <div class="py-22">
+    <ServicesList />
+  </div>
+</template>
+<script setup>
+const { t } = useI18n();
+</script>
