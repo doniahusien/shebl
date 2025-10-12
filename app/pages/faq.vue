9cf8a69f71b9737960cdf360641b5af2bacf261a
doniahusien <doniahusien@gmail.com>
2025-10-12 15:43:45 +0300

    feat(page): update FAQ page structure and translation links

--- app/pages/faq.vue
+++ app/pages/faq.vue
@@ -1,1 +1,18 @@
-<template></template>
+<script setup>
+const {t}= useI18n()
+</script>
+
+<template>
+
+    <BaseHero :title="t('faq.title')"
+    :subtitle="t('faq.subtitle')"
+    bgImage="bg-[url('/images/header4.jpg')]"
+  />
+    <div class="text-black p-20"> 
+      <FAQAccordion 
+      :indices="[0,1,2,3,4,5,6]"
+      contenStyle="text-black" 
+      labelStyle="text-black"/>
+    </div>
+ 
+</template>
