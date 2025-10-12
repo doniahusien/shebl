d5e80bd6f48f2527f2ec7353fe046ed0dcf96963
doniahusien <doniahusien@gmail.com>
2025-10-12 16:17:52 +0300

    feat(page): enhance Contact page layout and add new header components

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -1,1 +1,14 @@
-<template></template>
+<template>
+      <BaseHero
+    :title="t('contact.title')"
+    :subtitle="t('contact.description')"
+    bgImage="bg-[url('/images/contactheader.jpg')]"
+  />
+
+  <div class="px-20">
+  <ContactInfo/>
+  </div>
+</template>
+<script setup>
+const { t } = useI18n();
+</script>
