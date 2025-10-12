1bbe34429760607236d55deb432a44d06b8175cc
doniahusien <doniahusien@gmail.com>
2025-10-12 16:17:51 +0300

    feat(page): enhance About page layout and integrate BaseHero

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,3 +1,11 @@
 <template>
-    <h2>HIIIIII</h2>
+  <BaseHero
+    :title="t('about.title')"
+    :subtitle="t('about.subtitle')"
+    bgImage="bg-[url('/images/aboutheader.jpg')]"
+  />
+  <AboutUs :showBtn="false"/>
 </template>
+<script setup>
+const { t } = useI18n();
+</script>
