2e45574f70a36a398078b418c35cd4adae68943b
doniahusien <doniahusien@gmail.com>
2025-10-16 12:47:10 +0300

    update loader style

--- app/components/UI/Loader.vue
+++ app/components/UI/Loader.vue
@@ -1,24 +1,26 @@
 <template>
-<span class="loader"></span>
+  <div class="flex justify-center items-center min-h-screen bg-dark-gray">
+    <span class="loader"></span>
+  </div>
 </template>
 <style scoped>
 .loader {
-    width: 48px;
-    height: 48px;
-    border: 5px solid gray;
-    border-bottom-color: goldenrod;
-    border-radius: 50%;
-    display: inline-block;
-    box-sizing: border-box;
-    animation: rotation 1s linear infinite;
-    }
+  width: 48px;
+  height: 48px;
+  border: 5px solid gray;
+  border-bottom-color: goldenrod;
+  border-radius: 50%;
+  display: inline-block;
+  box-sizing: border-box;
+  animation: rotation 1s linear infinite;
+}
 
-    @keyframes rotation {
-    0% {
-        transform: rotate(0deg);
-    }
-    100% {
-        transform: rotate(360deg);
-    }
-    } 
+@keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
 </style>
