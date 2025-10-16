37ed8ac5dc993d130426e3ca5a1a857751a46600
doniahusien <doniahusien@gmail.com>
2025-10-16 12:34:43 +0300

    handle loader and error in home page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,29 +1,34 @@
 <template>
-  <section class="relative">
-    <Banner :hero="home?.data.banner" />
-    <div class="absolute bottom-[-4rem] left-1/2 -translate-x-1/2 w-[95%] z-20">
-      <HighLight :highlight="home?.data.banner.features"/>
-    </div>
-  </section>
-  <AboutUs :about="about" />
-  <div class="bg-mist-white"> 
-   <WhyUs :why="why"/>
+  <UILoader v-if="pending" />
+  <div v-else-if="error" class="text-center py-10 text-red-600">
+    <p>{{ error.message }}</p>
   </div>
+  <div v-else>
+    <section class="relative" >
+      <Banner :hero="home?.data.banner" />
+      <div class="absolute bottom-[-4rem] left-1/2 -translate-x-1/2 w-[95%] z-20">
+        <HighLight :highlight="home?.data.banner.features" />
+      </div>
+    </section>
+    <AboutUs :about="about" />
+    <div class="bg-mist-white">
+      <WhyUs :why="why" />
+    </div>
 
-  <Services :services="services" :features="services.features"/>
-  <FAQ :faq="faq"/>
-  <Contact :info="info"/>
+    <Services :services="services" :features="services.features" />
+    <FAQ :faq="faq" />
+    <Contact :info="info" />
+  </div>
 </template>
 <script setup>
-
 const { data: home, pending, error } = await useAsyncData("home", () =>
   useGlobalFetch("/preview")
 );
 
 const sections = computed(() => home.value?.data?.sections || []);
 const faq = computed(() => home.value?.data?.faq || []);
-const about = computed(() => sections.value.find(s => s.type == "about"));
-const why = computed(() => sections.value.find(s => s.type == "why_us"));
-const services = computed(() => sections.value.find(s => s.type == "our_services"));
-const info = computed(() => sections.value.find(s => s.type == "contact_info"));
+const about = computed(() => sections.value.find((s) => s.type == "about"));
+const why = computed(() => sections.value.find((s) => s.type == "why_us"));
+const services = computed(() => sections.value.find((s) => s.type == "our_services"));
+const info = computed(() => sections.value.find((s) => s.type == "contact_info"));
 </script>
