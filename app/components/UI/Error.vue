c0069c2fd789f9bad356b5aa5ad8acf1fd7a020d
doniahusien <doniahusien@gmail.com>
2025-10-16 13:21:59 +0300

    app/components/UI/Error.vue

---
+++ app/components/UI/Error.vue
@@ -0,0 +1,12 @@
+<template>
+     <div class="text-center py-10 text-red-600">
+    <p>{{ error.message }}</p>
+  </div>
+</template>
+<script setup>
+defineProps({
+    error: {
+        type:Object
+    }
+})
+</script>
