33d54d7a665d8debf0a018ff8409beefe65104a4
doniahusien <doniahusien@gmail.com>
2025-10-17 15:07:18 +0300

    Update About page content

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,38 +1,69 @@
 <template>
-   <UILoader v-if="pending" />
+  <UILoader v-if="pending" />
   <UIError v-else-if="error" :error="error" />
   <div v-else>
-  <BaseHero
-    :title="aboutBanner?.data.banner.title"
-    :subtitle="aboutBanner?.data.banner.description"
-    :bgImage="aboutBanner?.data.banner.image"
-  />
+    <BaseHero
+      :title="aboutBanner?.data?.banner?.title"
+      :subtitle="aboutBanner?.data?.banner?.description"
+      :bgImage="aboutBanner?.data?.banner?.image"
+    />
 
-  <AboutUs :showBtn="false" :about="about" />
-  <AboutUsGoals :goals="goals" />
+    <AboutUs :showBtn="false" :about="about" />
+    <AboutUsGoals :goals="goals" />
 
-  <div class="py-20 bg-mist-white">
-    <WhyUs :why="why" />
-  </div>
+    <div class="py-20 bg-mist-white">
+      <WhyUs :why="why" />
+    </div>
 
-  <div>
-    <AboutUsCoreValuesSection :core="core" />
-    <AboutUsVisionSection :vision="vision" />
-  </div>
+    <div>
+      <AboutUsCoreValuesSection :core="core" />
+      <AboutUsVisionSection :vision="vision" />
+    </div>
   </div>
 </template>
 
-<script setup>
-const { t } = useI18n();
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+import { computed } from 'vue'
 
-const { data: aboutBanner, pending, error } = await useAsyncData("about", () =>
-  useGlobalFetch("/preview?banner_type=about_banner")
-);
+const { t, locale } = useI18n()
 
-const sections = computed(() => aboutBanner.value?.data?.sections || []);
-const about = computed(() => sections.value.find((s) => s.type == "about"));
-const goals = computed(() => sections.value.find((s) => s.type == "goals"));
-const why = computed(() => sections.value.find((s) => s.type == "why_us"));
-const core = computed(() => sections.value.find((s) => s.type == "core_values"));
-const vision = computed(() => sections.value.find((s) => s.type == "our_vision"));
+interface Banner {
+  title?: string
+  description?: string
+  image?: string
+}
+
+interface Section {
+  type: string
+  [key: string]: any
+}
+
+interface AboutBannerResponse {
+  data?: {
+    banner?: Banner
+    sections?: Section[]
+  }
+}
+
+const { data: aboutBanner, pending, error } = await useAsyncData<AboutBannerResponse>(
+  'aboutData',
+  () => useGlobalFetch('/preview?banner_type=about_banner'),
+  { watch: [locale] }
+)
+watch(aboutBanner, (newVal) => {
+  if (newVal?.data?.banner) {
+    useDynamicMeta({
+      description: newVal.data.banner.description,
+      image: newVal.data.banner.image,
+    })
+  }
+})
+
+const sections = computed<Section[]>(() => aboutBanner.value?.data?.sections || [])
+const about = computed(() => sections.value.find(s => s.type === 'about'))
+const goals = computed(() => sections.value.find(s => s.type === 'goals'))
+const why = computed(() => sections.value.find(s => s.type === 'why_us'))
+const core = computed(() => sections.value.find(s => s.type === 'core_values'))
+const vision = computed(() => sections.value.find(s => s.type === 'our_vision'))
 </script>
