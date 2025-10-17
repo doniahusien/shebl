24de6aac9b385fb31d806c119684d7958c6b6254
doniahusien <doniahusien@gmail.com>
2025-10-17 15:07:30 +0300

    Add NoInternetConnection.vue component

---
+++ app/components/UI/NoInternetConnection.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class=" fixed top-0 left-0 w-screen h-screen flex flex-col justify-center items-center">
+        <div class=" space-y-6">
+
+            <h3 class="font-bold text-[25px] text-center">
+                {{ $t("TITLES.No internet connection") }}
+            </h3>
+            <img src="/connection.png" alt="connection" class="" />
+            <button class="base-btn m-auto" @click="reload">
+                {{ $t("BUTTONS.try again") }}
+            </button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const i18n = useI18n();
+function reload(){
+    location.reload();
+}
+</script>
