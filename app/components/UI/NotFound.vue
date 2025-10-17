5f6804543e0d09ec594b49dba378fcf60ae06fae
doniahusien <doniahusien@gmail.com>
2025-10-18 01:54:27 +0300

    update not found style

--- app/components/UI/NotFound.vue
+++ app/components/UI/NotFound.vue
@@ -1,25 +1,21 @@
 <template>
-  <div class="container my-20">
-    <div class="flex justify-center flex-col items-center">
+  <div class="m-auto bg-nav-bg pt-40  min-h-screen">
+    <div class="flex justify-center text-black flex-col items-center ">
       <img
-        width="300"
-        height="300"
+        width="400"
+        height="400"
         src="/cuate.png"
         alt="not found icon"
         loading="lazy"
       />
       <div class="error_section">
-        <h1 class="text-center font-bold text-2xl my-4 text-text">
+        <h1 class="text-center font-bold text-2xl my-4">
           {{ $t("something went wrong") }}
         </h1>
         <p class="text-center max-w-[500px] text-sub">
           {{ $t("pleaseKindlyTryAgain") }}
         </p>
-        <div class="flex justify-center mt-5">
-          <NuxtLink :to="localePath('/')" class="base-btn mx-2">
-            {{ $t("Back to home") }}
-          </NuxtLink>
-        </div>
+   
       </div>
     </div>
   </div>
