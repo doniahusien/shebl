9e7255d122eaadb39daa68daff7c18ab2c2626be
doniahusien <doniahusien@gmail.com>
2025-10-17 15:07:31 +0300

    Add NotFound.vue component

---
+++ app/components/UI/NotFound.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container my-20">
+    <div class="flex justify-center flex-col items-center">
+      <img
+        width="300"
+        height="300"
+        src="/cuate.png"
+        alt="not found icon"
+        loading="lazy"
+      />
+      <div class="error_section">
+        <h1 class="text-center font-bold text-2xl my-4 text-text">
+          {{ $t("something went wrong") }}
+        </h1>
+        <p class="text-center max-w-[500px] text-sub">
+          {{ $t("pleaseKindlyTryAgain") }}
+        </p>
+        <div class="flex justify-center mt-5">
+          <nuxt-link :to="localePath('/')" class="base-btn mx-2">
+            {{ $t("Back to home") }}
+          </nuxt-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const localePath = useLocalePath();
+</script>
