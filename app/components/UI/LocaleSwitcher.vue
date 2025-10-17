e7ec8dd3068842e0fb8f94ef7972f801cb4bff43
doniahusien <doniahusien@gmail.com>
2025-10-17 15:07:17 +0300

    Fix LocaleSwitcher.vue locale switch logic

--- app/components/UI/LocaleSwitcher.vue
+++ app/components/UI/LocaleSwitcher.vue
@@ -1,13 +1,17 @@
 <template>
   <button
-    @click="toggleLocale"
     class="flex justify-center items-center gap-1 cursor-pointer"
   >
     <NuxtImg src="/icons/langIcon.svg" alt="lang" class="w-5" />
-    <span>{{ locale == "en" ? "AR" : "EN" }}</span>
+    <NuxtLink :to="switchPath">
+      {{ t(`locale.${locale}`) }}
+    </NuxtLink>
   </button>
 </template>
 <script setup>
-const { locale, setLocale } = useI18n();
-const toggleLocale = () => setLocale(locale.value === "en" ? "ar" : "en");
+const { locale,t, setLocale } = useI18n();
+const switchLocalePath = useSwitchLocalePath()
+const targetLocale = computed(() => locale.value === 'ar' ? 'en' : 'ar')
+const switchPath = computed(() => switchLocalePath(targetLocale.value))
+/* const toggleLocale = () => setLocale(locale.value === "en" ? "ar" : "en"); */
 </script>
